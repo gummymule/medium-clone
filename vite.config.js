4f351d885e416ae1889da537e356a2cbf71f705b
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2025-05-26 13:10:39 +0700

    add dependencies

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            output: {
+                entryFileNames: `assets/[name].js`,
+                chunkFileNames: `assets/[name].js`,
+                assetFileNames: `assets/[name].[ext]`
+            }
+        }
+    }
 });
